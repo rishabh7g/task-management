ebb327f811f797765ff4d33586ae03e57cd87ebb
Rishabh7g <rishabh7g@gmail.com>
2024-06-26 21:52:24 +0530

    Make <AdminPage /> variant a11y compliant

--- src/pages/admin-page/AdminPage.tsx
+++ src/pages/admin-page/AdminPage.tsx
@@ -1,12 +1,14 @@
 const AdminPage = () => {
-  return (
-    <div className="w-full max-w-md space-y-8 rounded-xl bg-white p-10 shadow-lg">
-      <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-        Admin Page
-      </h2>
-      <p>This page is only accessible to users who have the role of admin.</p>
-    </div>
-  );
+    return (
+        <div className='w-full max-w-md space-y-8 rounded-xl bg-white p-10 shadow-lg'>
+            <h2 className='mt-6 text-center text-3xl font-extrabold text-gray-900'>
+                Admin Page
+            </h2>
+            <p className='text-center'>
+                Welcome admin, What would you like to do today?
+            </p>
+        </div>
+    );
 };
 
 export default AdminPage;
