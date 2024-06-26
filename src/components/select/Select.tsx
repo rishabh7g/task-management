4cfc05c4c8cfa4c2b1281079c60a95b31cd3b124
Rishabh7g <rishabh7g@gmail.com>
2024-06-26 21:42:49 +0530

    Make <Select /> variant a11y compliant

--- src/components/select/Select.tsx
+++ src/components/select/Select.tsx
@@ -1,41 +1,44 @@
+import React from 'react';
+
 interface SelectProps {
-  label: string;
-  name: string;
-  options: string[];
-  handleChange: (value: string) => void;
-  defaultOption?: string;
+    label: string;
+    name: string;
+    options: string[];
+    handleChange: (value: string) => void;
+    defaultOption?: string;
 }
 
 const Select = ({
-  label,
-  name,
-  options,
-  defaultOption = options[0],
-  handleChange,
+    label,
+    name,
+    options,
+    defaultOption,
+    handleChange,
 }: SelectProps) => {
-  return (
-    <div className="mb-4">
-      <label
-        className="block text-sm font-medium text-gray-700"
-        htmlFor={label.toLowerCase()}
-      >
-        {label}
-      </label>
-      <select
-        id={label.toLowerCase()}
-        name={name}
-        value={defaultOption}
-        onChange={(e) => handleChange(e.target.value)}
-        className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
-      >
-        {options.map((option) => (
-          <option key={option} value={option}>
-            {option}
-          </option>
-        ))}
-      </select>
-    </div>
-  );
+    return (
+        <div className='mb-4'>
+            <label
+                className='block text-sm font-medium text-gray-700'
+                htmlFor={name}
+            >
+                {label}
+            </label>
+            <select
+                id={name}
+                name={name}
+                value={defaultOption}
+                onChange={(e) => handleChange(e.target.value)}
+                className='mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm'
+                aria-label={label}
+            >
+                {options.map((option) => (
+                    <option key={option} value={option}>
+                        {option}
+                    </option>
+                ))}
+            </select>
+        </div>
+    );
 };
 
 export default Select;
