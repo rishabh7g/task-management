911ae2ec08eeda1232890fa451a45b94391731ba
Rishabh7g <rishabh7g@gmail.com>
2024-06-26 21:32:57 +0530

    Make <FormField /> variant a11y compliant

--- src/components/form-field/FormField.tsx
+++ src/components/form-field/FormField.tsx
@@ -1,92 +1,96 @@
-import classNames from "classnames";
-import { ReactNode } from "react";
-import Input, { AriaInvalidStatus } from "src/components/input/Input";
+import classNames from 'classnames';
+import { ReactNode } from 'react';
+import Input, { AriaInvalidStatus } from 'src/components/input/Input';
 
 interface FormFieldProps {
-  id: string;
-  label: ReactNode;
-  name: string;
-  type: string;
-  value: string;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  htmlRef?: React.RefObject<HTMLInputElement>;
-  autocomplete?: string;
-  required?: boolean;
-  isValid: boolean;
-  onFocus: () => void;
-  onBlur: () => void;
-  InputHelperContent: ReactNode;
-  placeholder: string;
-  isFocused: boolean;
+    id: string;
+    label: ReactNode;
+    name: string;
+    type: string;
+    value: string;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    htmlRef?: React.RefObject<HTMLInputElement>;
+    autocomplete?: string;
+    required?: boolean;
+    isValid: boolean;
+    onFocus: () => void;
+    onBlur: () => void;
+    InputHelperContent: ReactNode;
+    placeholder: string;
+    isFocused: boolean;
 }
 
 export const FormField = ({
-  id,
-  label,
-  name,
-  type,
-  value,
-  onChange,
-  htmlRef,
-  autocomplete,
-  required = false,
-  isValid,
-  onFocus,
-  onBlur,
-  InputHelperContent,
-  placeholder = "",
-  isFocused,
+    id,
+    label,
+    name,
+    type,
+    value,
+    onChange,
+    htmlRef,
+    autocomplete,
+    required = false,
+    isValid,
+    onFocus,
+    onBlur,
+    InputHelperContent,
+    placeholder = '',
+    isFocused,
 }: FormFieldProps) => {
-  return (
-    <div className="my-7 flex flex-col gap-2">
-      <Input
-        id={id}
-        label={label}
-        name={name}
-        type={type}
-        value={value}
-        onChange={onChange}
-        htmlRef={htmlRef}
-        autocomplete={autocomplete}
-        required={required}
-        ariaInvalid={isValid ? AriaInvalidStatus.FALSE : AriaInvalidStatus.TRUE}
-        ariaDescribedby={`${id}-error`}
-        onFocus={onFocus}
-        onBlur={onBlur}
-        placeholder={placeholder}
-      />
-      <InputHelper
-        isVisible={isFocused && !!value && !isValid}
-        isHidden={!isFocused || !value || isValid}
-        content={InputHelperContent}
-        id={`${id}-error`}
-      />
-    </div>
-  );
+    return (
+        <div className='my-7 flex flex-col gap-2'>
+            <Input
+                id={id}
+                label={label}
+                name={name}
+                type={type}
+                value={value}
+                onChange={onChange}
+                htmlRef={htmlRef}
+                autocomplete={autocomplete}
+                required={required}
+                ariaInvalid={
+                    isValid ? AriaInvalidStatus.FALSE : AriaInvalidStatus.TRUE
+                }
+                ariaDescribedby={`${id}-error`}
+                onFocus={onFocus}
+                onBlur={onBlur}
+                placeholder={placeholder}
+            />
+            <InputHelper
+                isVisible={isFocused && !!value && !isValid}
+                isHidden={!isFocused || !value || isValid}
+                content={InputHelperContent}
+                id={`${id}-error`}
+            />
+        </div>
+    );
 };
 
 interface InputHelperProps {
-  isVisible: boolean;
-  isHidden: boolean;
-  content: ReactNode;
-  id: string;
+    isVisible: boolean;
+    isHidden: boolean;
+    content: ReactNode;
+    id: string;
 }
 
 const InputHelper = ({
-  isVisible,
-  isHidden,
-  content,
-  id,
+    isVisible,
+    isHidden,
+    content,
+    id,
 }: InputHelperProps) => {
-  return (
-    <div
-      id={id}
-      className={classNames("rounded bg-amber-100 p-5", {
-        visible: isVisible,
-        hidden: isHidden,
-      })}
-    >
-      {content}
-    </div>
-  );
+    return (
+        <div
+            id={id}
+            role='alert'
+            className={classNames('rounded bg-amber-100 p-5', {
+                visible: isVisible,
+                hidden: isHidden,
+            })}
+            aria-live='assertive'
+        >
+            {content}
+        </div>
+    );
 };
