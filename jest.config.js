79e2c7cd2127dd577a60730ba94a2997f8b558bc
Rishabh7g <rishabh7g@gmail.com>
2024-07-05 14:40:17 +1000

    Fix @testing-library/jest-dom error

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,15 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-   preset: 'ts-jest',
-   testEnvironment: 'jsdom', // For testing React components in a browser-like environment
-   setupFilesAfterEnv: ['<rootDir>/jest/jest.setup.ts'],
-   moduleNameMapper: {
-      'src/(.*)$': '<rootDir>/src/$1',
-   },
-   testEnvironmentOptions: {
-      customExportConditions: [''],
-   },
+    preset: 'ts-jest',
+    rootDir: '.',
+    testEnvironment: 'jest-environment-jsdom',
+    displayName: 'frontend',
+    moduleDirectories: ['node_modules', 'src'],
+    setupFilesAfterEnv: ['@testing-library/jest-dom'],
+    moduleNameMapper: {
+        'src/(.*)$': '<rootDir>/src/$1',
+    },
+    testEnvironmentOptions: {
+        customExportConditions: [''],
+    },
 };
