05a1f706ede5d459d91d6a270a5348d015d8959f
Rishabh7g <rishabh7g@gmail.com>
2024-07-06 21:06:36 +1000

    remove unused

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { AuthProvider } from 'src/context/auth-context';
-import '@testing-library/jest-dom';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement,
