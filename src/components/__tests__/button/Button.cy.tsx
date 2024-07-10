4befbfc02f0107e8736732897a619f3e9180eda0
Rishabh7g <rishabh7g@gmail.com>
2024-07-10 14:41:23 +1000

    Add a test case

---
+++ src/components/__tests__/button/Button.cy.tsx
@@ -0,0 +1,8 @@
+import { mount } from 'cypress/react18';
+import { PrimaryButton } from 'src/components/button/Button';
+
+describe('Button comonent testing', () => {
+    it('renders card content', () => {
+        mount(<PrimaryButton label='Click orange juice' />);
+    });
+});
