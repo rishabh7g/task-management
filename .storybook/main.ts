5b52fa33bf762e4ee3ce8bdd495cf54be98e38eb
Rishabh7g <rishabh7g@gmail.com>
2024-07-10 16:42:19 +1000

    Fix the issue of tailwind compatibility with storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -9,6 +9,14 @@ const config: StorybookConfig = {
         '@storybook/addon-essentials',
         '@chromatic-com/storybook',
         '@storybook/addon-interactions',
+        {
+            name: '@storybook/addon-postcss',
+            options: {
+                postcssLoaderOptions: {
+                    implementation: require('postcss'),
+                },
+            },
+        },
     ],
     framework: {
         name: '@storybook/react-webpack5',
