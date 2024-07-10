998b37af189efbbe6d02b2b8ce79a7a8c7e43ee7
Rishabh7g <rishabh7g@gmail.com>
2024-07-10 16:42:31 +1000

    Add storybook for <ErrorMessage />

--- src/components/error-message/ErrorMessage.stories.tsx
+++ src/components/error-message/ErrorMessage.stories.tsx
@@ -1,7 +1,6 @@
 import { Meta, StoryObj } from '@storybook/react';
 import { ErrorMessage } from './ErrorMessage';
 
-// Meta configuration for the Storybook
 const meta: Meta<typeof ErrorMessage> = {
     title: 'Example/ErrorMessage',
     component: ErrorMessage,
@@ -22,18 +21,14 @@ export default meta;
 
 type Story = StoryObj<typeof ErrorMessage>;
 
-// Default story
 export const Default: Story = {
     args: {
-        errorMessage: 'This is an error message.',
-        htmlRef: { current: null }, // If you want to provide a ref example
+        errorMessage: 'This is an error message',
     },
 };
 
-// Example story with empty error message
 export const EmptyError: Story = {
     args: {
         errorMessage: '',
-        htmlRef: { current: null }, // If you want to provide a ref example
     },
 };
