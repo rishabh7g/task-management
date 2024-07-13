2008b3df2bfd7b463fb0b7195fa096bfe75a4c7a
Rishabh7g <rishabh7g@gmail.com>
2024-07-13 13:33:05 +1000

    Disable react-dev-tools

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { AuthProvider } from 'src/context/auth-context';
 import { App } from './App';
 import './index.css';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
+const isEnvProd = process.env.NODE_ENV === 'production';
+
+if (isEnvProd) {
+    disableReactDevTools();
+}
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement,
