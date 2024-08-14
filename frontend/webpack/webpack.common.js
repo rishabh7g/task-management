16cb7f82eae5211bbeaa6ea0597d4705d9e31184
Rishabh7g <rishabh7g@gmail.com>
2024-08-15 07:36:24 +1000

    Configure webpack to use chunk effectively

--- frontend/webpack/webpack.common.js
+++ frontend/webpack/webpack.common.js
@@ -1,51 +1,58 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const Dotenv = require("dotenv-webpack");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-  entry: path.resolve(
-    __dirname,
-    "..",
-    "./src/index.tsx",
-  ) /* tell webpack where is the entry point */,
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-    alias: {
-      src: path.resolve(__dirname, "..", "./src"),
+    entry: path.resolve(__dirname, '..', './src/index.tsx'),
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+        alias: {
+            src: path.resolve(__dirname, '..', './src'),
+        },
     },
-  } /* resolve imports, it will first look for .tsx, then .ts, and then .js */,
-  module: {
-    rules: [
-      {
-        test: /\.(ts|js)x?$/,
-        exclude: /node_modules/,
-        use: [{ loader: "babel-loader" }],
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader", "postcss-loader"],
-      },
-      {
-        test: /\.(?:ico|gif|png|jpg|jpeg)$/,
-        type: "asset/resource",
-      },
-      {
-        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-        type: "asset/inline",
-      },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/,
+                exclude: /node_modules/,
+                use: [{ loader: 'babel-loader' }],
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader', 'postcss-loader'],
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: 'asset/inline',
+            },
+        ],
+    },
+    output: {
+        path: path.resolve(__dirname, '..', './build'),
+        filename: '[name].[contenthash].js',
+        chunkFilename: '[name].[contenthash].js',
+        clean: true,
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+            minSize: 0,
+        },
+    },
+    performance: {
+        maxAssetSize: 500 * 1024,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, '..', './src/index.html'),
+        }),
+        new Dotenv({
+            path: path.resolve(__dirname, '..', './.env'), // Path to .env file (this is the default)
+            systemvars: true, // Load system environment variables as well
+        }),
     ],
-  } /* define module rules */,
-  output: {
-    path: path.resolve(__dirname, "..", "./build"),
-    filename: "bundle.js",
-  } /* where to store all the build files */,
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "..", "./src/index.html"),
-    }),
-    new Dotenv({
-      path: path.resolve(__dirname, "..", "./.env"), // Path to .env file (this is the default)
-      systemvars: true, // Load system environment variables as well
-    }),
-  ] /* define plugins */,
 };
